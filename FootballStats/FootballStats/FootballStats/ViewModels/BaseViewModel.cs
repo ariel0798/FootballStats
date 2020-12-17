3f79293ba0c0e41b36e7e97a7eb3201624a44679
ariel0798 <arielcruz07@hotmail.com>
2020-12-17 12:18:20 -0400

    Base viewmodel with property changed

---
+++ FootballStats/FootballStats/FootballStats/ViewModels/BaseViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace FootballStats.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
