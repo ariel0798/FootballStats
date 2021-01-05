30efb85bc3f551da2d9d5a2b9183799672e74a5b
cAugustoSantana <c300698@gmail.com>
2021-01-05 13:35:02 -0400

    0.1(Not Working)

--- FootballStats/FootballStats/FootballStats/ViewModels/HomePageViewModel.cs
+++ FootballStats/FootballStats/FootballStats/ViewModels/HomePageViewModel.cs
@@ -1,4 +1,9 @@
-﻿using FootballStats.Services.Interfaces;
+﻿using Acr.UserDialogs;
+using FootballStats.Models.Fixtures;
+using FootballStats.Services.Interfaces;
+using Newtonsoft.Json;
+using Prism.Commands;
+using System.Threading.Tasks;
 
 namespace FootballStats.ViewModels
 {
@@ -7,5 +12,28 @@ namespace FootballStats.ViewModels
         public HomePageViewModel(IApiManager apiManager) : base(apiManager)
         {
         }
+
+        public Fixture Fixture {get;set;}
+
+        public DelegateCommand GetFixtureDataCommand => new DelegateCommand(async () => await RunSafe(GetData()));
+
+
+        async Task GetData() 
+        {
+            var footballResponse = await ApiManager.GetFixturesLive();
+
+            if (footballResponse.IsSuccessStatusCode) 
+            {
+                var jsonResponse = await footballResponse.Content.ReadAsStringAsync();
+                var fixtures = await Task.Run(() => JsonConvert.DeserializeObject<Fixtures>(jsonResponse));
+                Fixture = fixtures.Api.Fixtures[0];
+            }
+            else 
+            {
+                await PageDialogs.AlertAsync("Unable to get data", "Error", "Ok");
+            }
+        }
+        
+
     }
 }
