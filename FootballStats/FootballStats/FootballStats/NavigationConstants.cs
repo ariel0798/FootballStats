f9915bc75acafe02afebd4f31e5ffb71156f39b2
Richard <arielcruz0798@gmail.com>
2021-01-08 04:05:40 -0400

    Merge conflicts HomeFeature to Develop solved

--- FootballStats/FootballStats/FootballStats/NavigationConstants.cs
+++ FootballStats/FootballStats/FootballStats/NavigationConstants.cs
@@ -7,11 +7,9 @@ namespace FootballStats
         public const string TrophyPage = "TrophyPage";
         public const string LeaguePage = "LeaguePage";
         public const string HomePage = "HomePage";
-<<<<<<< HEAD
         public const string TeamStatsPage = "TeamStatsPage";
         public const string PlayerPage = "PlayerPage";
-=======
         public const string FixturePage = "FixturePage";
->>>>>>> HomeFeauture
+
     }
 }
