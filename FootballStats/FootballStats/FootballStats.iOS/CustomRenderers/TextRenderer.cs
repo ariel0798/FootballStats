8cb1bea2f30e73b63946c9e365d35bd0572bc435
cAugustoSantana <c300698@gmail.com>
2021-01-14 22:41:57 -0400

    CustomRenderer

---
+++ FootballStats/FootballStats/FootballStats.iOS/CustomRenderers/TextRenderer.cs
@@ -0,0 +1,24 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Label),typeof(LabelRenderer))]
+namespace FootballStats.iOS.CustomRenderers
+{
+   public class TextRenderer : Xamarin.Forms.Platform.iOS.LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            if (Control != null)
+            {
+                Control.BackgroundColor = UIColor.FromRGB(66, 66, 66);
+             
+            }
+        }
+    }
+}
